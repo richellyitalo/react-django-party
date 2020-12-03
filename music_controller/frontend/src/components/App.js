f6042d4748ad81dfb1be5f5d91bb79874edef022
Richelly Italo <richellyitalo@gmail.com>
2020-12-02 22:17:31 -0300

    ✨ adding app frontend with reactjs

---
+++ music_controller/frontend/src/components/App.js
@@ -0,0 +1,12 @@
+import React, { Component } from 'react';
+
+
+export default class App extends Component {
+  constructor(props) {
+    super(props)
+  }
+
+  render() {
+    return <h1>Olá brasil!</h1>
+  }
+}
