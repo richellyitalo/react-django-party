9c75069e3995dd47f839baf3797f0ed24bae0fc3
Richelly Italo <richellyitalo@gmail.com>
2020-12-06 15:07:30 -0300

    ✨ creating room

--- music_controller/api/views.py
+++ music_controller/api/views.py
@@ -1,8 +1,43 @@
-from rest_framework import generics
+from rest_framework import generics, status
+from rest_framework.views import APIView
+from rest_framework.response import Response
+
 from .models import Room
-from .serializers import RoomSerializer
+from .serializers import RoomSerializer, CreateRoomSerializer
 
 
 class RoomView(generics.ListAPIView):
     queryset = Room.objects.all()
     serializer_class = RoomSerializer
+
+
+class CreateRoomView(APIView):
+    serializer_class = CreateRoomSerializer
+
+    def post(self, request, format=None):
+        serializer = self.serializer_class(data=request.data)
+
+        if not serializer.is_valid():
+            payload = {'Bad Request': 'Invalid data', 'erros': serializer.errors}
+            return Response(payload, status.HTTP_400_BAD_REQUEST)
+
+        guest_can_pause = serializer.data['guest_can_pause']
+        votes_to_skip = serializer.data['votes_to_skip']
+
+        if not request.session.exists(request.session.session_key):
+            request.session.create()
+        host = request.session.session_key
+
+        queryset = Room.objects.filter(host=host)
+        if queryset.exists():
+            room = queryset[0]
+            room.guest_can_pause = guest_can_pause
+            room.votes_to_skip = votes_to_skip
+            room.save(update_fields=['guest_can_pause', 'votes_to_skip'])
+            return Response(RoomSerializer(room).data, status.HTTP_200_OK)
+        else:
+            room = Room(host=host, guest_can_pause=guest_can_pause,
+                        votes_to_skip=votes_to_skip)
+            room.save()
+
+        return Response(RoomSerializer(room).data, status.HTTP_201_CREATED)
--- music_controller/frontend/src/pages/HomePage/HomePage.js
+++ music_controller/frontend/src/pages/HomePage/HomePage.js
@@ -1,7 +1,15 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 export default class HomePage extends Component {
   render () {
-    return <h1>Home page.</h1>
+    return (
+      <div>
+        <h1>Home page.</h1>
+        <p>
+          <Link to="/join">Ir para Join</Link>
+        </p>
+      </div>
+    )
   }
 }
